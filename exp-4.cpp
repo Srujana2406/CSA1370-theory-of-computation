9878088b1e1de72f4b1c4a9c31a1cbe7f8750e52
Srujana2406 <129171029+Srujana2406@users.noreply.github.com>
2024-09-25 21:01:39 +0530

    Add files via upload

---
+++ exp-4.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+	char string[100];
+	int len;
+	printf("enter the string:");
+	scanf("%s",string);
+	len=strlen(string);
+	if(string[0]=='0'&&string[len-1]=='0'||string[0]=='1'&&string[len-1]=='1')
+	{
+		for(int i=0;i<len;i++)
+		{
+			if(string[i]>'1')
+			{
+				printf("invalid");
+				return 0;
+			}
+       }
+     printf("The string is accepted");
+}
+    else 
+	{
+        printf("The string is not accepted.");
+    }
+
+    return 0;
+}
+
---
+++ exp-8.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+	char string[100];
+	int len;
+	printf("enter the string:");
+	scanf("%s",string);
+	len=strlen(string);
+	if(string[0]=='b'&&string[len-1]=='a')
+	{
+		for(int i=0;i<len;i++)
+		{
+			if(string[i]=='0'||string[i]=='1')
+			{
+				printf("invalid");
+				return 0;
+			}
+       }
+     printf("The string is accepted");
+}
+    else 
+	{
+        printf("The string is not accepted.");
+    }
+
+    return 0;
+}
+
